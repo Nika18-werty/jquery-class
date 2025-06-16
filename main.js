3c7e9d0057de82bffb40c94b8248a1b6e75dd79b
Nika18-werty <Demch_mb30@student.itstep.org>
2025-06-16 19:40:40 +0300

    traffic light

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
+// $(document).ready(function () {
+//   const colors = [
+//     '#ff5733', '#33ff57', '#3357ff', '#f1c40f',
+//     '#8e44ad', '#16a085', '#e67e22', '#1abc9c',
+//     '#2c3e50', '#d35400', '#c0392b', '#7f8c8d'
+//   ];
+
+//   $('#add-block').click(function () {
+//     const randomColor = colors[Math.floor(Math.random() * colors.length)];
+//     const $block = $('<div class="block"></div>').css('background-color', randomColor);
+
+//     $block.click(function () {
+//       $(this).remove();
+//     });
+
+//     $('#container').append($block);
+//   });
+// });
+
+
 $(document).ready(function () {
-  const colors = [
-    '#ff5733', '#33ff57', '#3357ff', '#f1c40f',
-    '#8e44ad', '#16a085', '#e67e22', '#1abc9c',
-    '#2c3e50', '#d35400', '#c0392b', '#7f8c8d'
-  ];
+  const lights = ['#red', '#yellow', '#green'];
+  let current = 0;
 
-  $('#add-block').click(function () {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const $block = $('<div class="block"></div>').css('background-color', randomColor);
+  function updateLight() {
+    $('.light').removeClass('active');
 
-    $block.click(function () {
-      $(this).remove();
-    });
+    $(lights[current]).addClass('active');
+  }
 
-    $('#container').append($block);
+  $('#next').click(function () {
+    current = (current + 1) % lights.length; 
+    updateLight();
   });
+
+  updateLight(); 
 });
+
+
